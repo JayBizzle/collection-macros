54d3d8760b1dd116ed1e2c717630e4d43187352d
Mark Beech <mbeech@mark-beech.co.uk>
2016-06-23 22:00:20 +0100

    Create maxBy.php

---
+++ maxBy.php
@@ -0,0 +1,10 @@
+<?php
+Collection::macro('maxBy', function ($callback) {
+    $callback = $this->valueRetriever($callback);
+    return $this->reduce(function ($result, $item) use ($callback) {
+        if ($result === null) {
+            return $item;
+        }
+        return $callback($item) > $callback($result) ? $item : $result;
+    });
+});
